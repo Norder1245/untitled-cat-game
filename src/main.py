51c88287d409682556a58fd652b9d94c8e12175c
Jack <elijah.sjogren@gmail.com>
2025-03-06 12:13:06 -0700

    add: implement event loop to handle window close in Pygame

--- src/main.py
+++ src/main.py
@@ -6,3 +6,8 @@ pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("MEOW")
 pygame.draw.circle(screen, RED, (400,300), 50)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
