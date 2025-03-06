4ef4a676282ca0631c63cad713bb57591c68414f
Jack <elijah.sjogren@gmail.com>
2025-03-06 12:14:05 -0700

    add: update display to show drawn circle in Pygame

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("MEOW")
 pygame.draw.circle(screen, RED, (400,300), 50)
+pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
