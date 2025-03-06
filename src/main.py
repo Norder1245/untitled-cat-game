5af546b7b38597ef274566797e7bb308a640c09b
Jack <elijah.sjogren@gmail.com>
2025-03-06 12:40:34 -0700

    fix: clear console and add welcome message; handle invalid color input

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-import pygame
+import pygame, os
 import sys
 from PyCol import *
 from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("MEOW")
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Hello, welcome to the circle thingy")
 col = input("What color? ")
 if col == "red":
     col = RED
@@ -12,6 +14,9 @@ elif col == "green":
     col = GREEN
 elif col == "blue":
     col = BLUE
+else :
+    print("Invalid color")
+    exit()
 pygame.draw.circle(screen, col, (400,300), 50)
 pygame.display.flip()
 running = True
