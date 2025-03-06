84ab23a1517830331e26107e19ea1161f381a889
Jack <elijah.sjogren@gmail.com>
2025-03-06 12:22:49 -0700

    add: prompt user for circle color selection in Pygame

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("MEOW")
+col = input("What color?")
+if col == "red":
+    col = RED
+elif col == "green":
+    col = GREEN
+elif col == "blue":
+    col = BLUE
 pygame.draw.circle(screen, RED, (400,300), 50)
 pygame.display.flip()
 running = True
